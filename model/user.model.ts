5a8f4a65f06d69d37a47ebea8688e2795570fd9a
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-11-17 21:41:19 +0500

    added user modal

---
+++ model/user.model.ts
@@ -0,0 +1,79 @@
+import { DataTypes, Model } from "sequelize";
+import sequelize from "../config/db.js";
+
+export class User extends Model {
+  first_name!: string;
+  last_name?: string;
+  email!: string;
+  password!: string;
+  role!: string;
+  phone_number?: string;
+  otp?: number;
+  otp_time?: number;
+  verified?: boolean;
+  created_at!: Date;
+  updated_at!: Date;
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.ENUM,
+      allowNull: true,
+      defaultValue: 'customer'
+    },
+    phone_number: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    otp: {
+      type: DataTypes.NUMBER,
+      allowNull: true,
+      defaultValue: null
+    },
+    otp_time: {
+      type: DataTypes.NUMBER,
+      allowNull: true,
+      defaultValue: null
+    },
+    verified: {
+      type: DataTypes.BOOLEAN,
+      allowNull: true,
+      defaultValue: false
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+  },
+  {
+    tableName: "user",
+    timestamps: true,
+    sequelize: sequelize,
+  }
+);
