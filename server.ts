737e931312cb62c9c0ad059929e325f9b5c940a6
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-11-17 22:31:15 +0500

    import userRouter server

--- server.ts
+++ server.ts
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
+import userRouter from "./router/user.router.js"
 
 const app = express()
 dotenv.config()
@@ -9,6 +10,7 @@ app.use(cors())
 const PORT = process.env.PORT || 3000
 
 // router
+app.use(userRouter)
 
 app.listen(PORT, () => {
     console.log("Server is running at: ", PORT);
