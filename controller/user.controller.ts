9c99a5bc0437aef17977298efa53a6200e08fe6d
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-11-18 11:26:08 +0500

    added function registerUser user.controller

--- controller/user.controller.ts
+++ controller/user.controller.ts
@@ -1,16 +1,38 @@
-import type { NextFunction, Request, Response } from "express";
+import type { Request, Response, NextFunction } from "express";
+import bcrypt from "bcryptjs";
 import { User } from "../model/user.model.js";
+import { generateAccessToken, generateRefreshToken } from "../utils/token.js";
+import { sendOtp } from "../utils/send-otp.js";
 
-User.sync({ force: false });
-
-export const registerUser = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-): Promise<Response | void> => {
+export const registerUser = async (req: Request, res: Response, next: NextFunction) => {
   try {
+    const { first_name, last_name, email, password } = req.body;
 
-  } catch (error: any) {
+    const existingEmail = await User.findOne({ where: { email } });
+    if (existingEmail) {
+      return res.status(400).json({ message: "Bu email band!" });
+    }
+
+    const otp = Math.floor(100000 + Math.random() * 900000).toString();
+    const otp_time = Date.now() + 2 * 60 * 1000;
+
+    const hashed = await bcrypt.hash(password, 12);
+
+    await User.create({
+      first_name,
+      last_name,
+      email,
+      password: hashed,
+      otp,
+      otp_time,
+      verified: false,
+    });
+
+    await sendOtp(email, otp);
+
+    return res.status(201).json({ message: "Ro'yxatdan o'tdingiz! Emailga kod yuborildi." });
+
+  } catch (error) {
     next(error);
   }
 };
