3e38a4d294160482306c6212a9fdf2de63942b5d
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-11-18 11:17:26 +0500

    update send otp catch error

---
+++ utils/send-otp.ts
@@ -0,0 +1,26 @@
+import nodemailer  from "nodemailer"
+
+module.exports = async function(email: string, otp: string) {
+    try {
+        const transport = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: "samigullayevlutfulla@gmail.com",
+                pass: process.env.APP_PASS
+            }
+        })
+        await transport.sendMail({
+            from: "samigullayevlutfulla@gmail.com",
+            to:email,
+            subject: "Exclusive",
+            text: "Verification code from exclusive",
+            html: `<p style="font-size: 24px">Verify code: <strong style="color: green">${otp}</strong></p>`
+        })
+    } catch (error) {
+        if (error instanceof Error) {
+            throw new Error(error.message);
+        }
+        throw new Error("Unknown error occurred");
+    }
+}
+
