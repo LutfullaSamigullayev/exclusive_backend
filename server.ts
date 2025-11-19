7a5ce6e730529fcbda84a6750de215379c794f2f
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-11-19 08:29:24 +0500

    import cookieParser and use server

--- server.ts
+++ server.ts
@@ -1,12 +1,14 @@
 import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
 import userRouter from "./router/user.router.js"
 
 const app = express()
 dotenv.config()
 app.use(express.json())
 app.use(cors())
+app.use(cookieParser())
 const PORT = process.env.PORT || 3000
 
 // router
