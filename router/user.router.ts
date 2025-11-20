cf33a1620692b42c5a00939ae70775d03dfb0e64
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-11-20 10:26:13 +0500

    added user router api

--- router/user.router.ts
+++ router/user.router.ts
@@ -1,9 +1,48 @@
 import { Router, type RequestHandler } from "express";
-import { registerUser } from "../controller/user.controller.js";
+import {
+  registerUser,
+  verifyUser,
+  loginUser,
+  forgotPassword,
+  resetPassword,
+  logoutUser,
+  toAdmin,
+  toSeller,
+  refreshAccessToken,
+} from "../controller/user.controller.js";
+import { authorization } from "../middleware/authorization.middleware.js";
+import { superAdminChecker } from "../middleware/super_admin.chesker.middleware.js";
+import { adminOrSuperAdminChecker } from "../middleware/admin-super_admin.chesker.middleware.js";
+import { refreshTokenMiddleware } from "../middleware/refresh.token.middleware.js";
 
-const userRouter = Router()
+const userRouter = Router();
 
-userRouter.get("/register", registerUser as RequestHandler)
+userRouter.post("/auth/register", registerUser as RequestHandler);
+userRouter.post("/auth/verify", verifyUser as RequestHandler);
+userRouter.post("/auth/login", loginUser as RequestHandler);
+userRouter.post("/auth/forgot-password", forgotPassword as RequestHandler);
+userRouter.post("/auth/reset-password", resetPassword as RequestHandler);
 
-export default userRouter
+userRouter.post("/auth/logout", authorization as RequestHandler, logoutUser as RequestHandler);
+userRouter.post(
+  "/auth/refresh",
+  refreshTokenMiddleware as RequestHandler,
+  refreshAccessToken as RequestHandler
+);
+
+userRouter.post(
+  "/users/to-admin",
+  authorization as RequestHandler,
+  superAdminChecker as RequestHandler,
+  toAdmin as RequestHandler
+);
+
+userRouter.post(
+  "/users/to-seller",
+  authorization as RequestHandler,
+  adminOrSuperAdminChecker as RequestHandler,
+  toSeller as RequestHandler
+);
+
+export default userRouter;
 
