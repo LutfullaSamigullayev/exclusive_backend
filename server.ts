7ab936e4f281eedb93f79d6b50e600d27be45068
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-11-21 08:30:53 +0500

    update server

--- server.ts
+++ server.ts
@@ -3,6 +3,13 @@ import cors from "cors"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
 import userRouter from "./router/user.router.js"
+import categoryRouter from "./router/category.router.js"
+import productRouter from "./router/product.router.js"
+import favoriteRouter from "./router/favorite.router.js"
+import cartRouter from "./router/cart.router.js"
+import orderRouter from "./router/order.router.js"
+import sequelize from "./config/db.js"
+import { User, Product, Category, Favorite, Cart, Order, OrderItem } from "./model/index.js"
 
 const app = express()
 dotenv.config()
@@ -13,8 +20,17 @@ const PORT = process.env.PORT || 3000
 
 // router
 app.use(userRouter)
+app.use(categoryRouter)
+app.use(productRouter)
+app.use(favoriteRouter)
+app.use(cartRouter)
+app.use(orderRouter)
 
-app.listen(PORT, () => {
-    console.log("Server is running at: ", PORT);
+;(async () => {
+    await sequelize.sync({ force: false })
+    app.listen(PORT, () => {
+        console.log("Server is running at: ", PORT);
+    })
+})().catch((error) => {
+    console.log(error)
 })
-
